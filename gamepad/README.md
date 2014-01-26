ff03439e114c1dde7c7208143d6cff37bd60657a
Herve Senot <herve.senot@groundtruth.com.au>
2014-01-26 12:11:02 +1100

    Added gamepad control library (node.js/express/socket.io)

---
+++ gamepad/README.md
@@ -0,0 +1,17 @@
+  
+# GamePad Demo
+# from https://github.com/teemujonkkari/gamepad-demo
+
+GamePad demo shows how to use mobile phone browser as gamepad for playing game in your desktop browser.
+
+## How to test
+
+1. Install Node.js
+2. Download GamePad demo to some folder
+3. Browse to specified folder with Terminal/Console
+4. Install required Node.js modules: $ npm install socket.io express
+5. Fix right ip address for socket connection in gamepad.js
+5. Launch demo: $ sudo node server.js (port 80)
+6. Open game with browser: http://172.0.0.1/
+7. Browse with your mobile browser to your computer's network address (http://192.168.1.20/gamepad/) to use the gamepad.
+
---
+++ gamepad/server.js
@@ -0,0 +1,38 @@
+var express = require('express'),
+    app = express()
+  , http = require('http')
+  , server = app.listen(8888)
+  , io = require('socket.io').listen(server);
+
+app.use('/assets', express.static(__dirname + '/assets'));
+
+
+  io.enable('browser client minification');  // send minified client
+  io.enable('browser client etag');          // apply etag caching logic based on version number
+  io.enable('browser client gzip');          // gzip the file
+  io.set('log level', 0);                    // reduce logging
+
+app.get('/', function (req, res) {
+  res.sendfile(__dirname + '/index.html');
+});
+
+app.get('/gamepad/', function (req, res) {
+  res.sendfile(__dirname + '/gamepad/index.html');
+});
+
+io.sockets.on('connection', function (socket) {
+
+  socket.on('gamepad', function (data) {
+    //console.log(data) 
+    socket.broadcast.emit('gamepad', data); //JSON.parse(data));
+  });
+
+  socket.on('connect', function () {
+    socket.broadcast.emit('user connected');
+  });
+
+  socket.on('disconnect', function () { 
+    socket.broadcast.emit('user disconnected');
+  });
+
+});
